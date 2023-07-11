e242af6c9acbc6b6e696bc2a4ec0744410407133
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-07-11 12:19:23 +0200

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -1,9 +1,9 @@
 output "pub" {
   value     = tls_private_key.SSH.public_key_pem
-  sensitive = true
+  sensitive = false
 }
 
 output "private" {
   value = tls_private_key.SSH.private_key_pem
-  sensitive = true
+  sensitive = false
 }
