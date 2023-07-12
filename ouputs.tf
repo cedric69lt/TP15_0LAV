f34acfbe91c55cb34cab49e6898a30330397edf6
lingcode72 <66947285+lingcode72@users.noreply.github.com>
2023-07-12 11:03:47 +0200

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -7,3 +7,7 @@ output "private" {
   value = tls_private_key.SSH.private_key_pem
   sensitive = true
 }
+
+output "public_ip_address" {
+  value = "${data.azurerm_public_ip.buildserver-pip.ip_address}"
+}
