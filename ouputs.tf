6a0932a047b40d95f7078865c5117153aa62b588
lingcode72 <66947285+lingcode72@users.noreply.github.com>
2023-07-17 11:37:40 +0200

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -12,14 +12,14 @@ output "public_ips" {
   value = [for instance in azurerm_public_ip.test : instance.name]
 }
 
-output "IP_worker_0" {
-  value = IP_pub_worker_0
+output "IP_pub_worker_0" {
+  value = azurerm_public_ip.test[0].ip_address
 }
 
-output "IP_worker_1" {
-  value = IP_pub_worker_1
+output "IP_pub_worker_1" {
+  value = azurerm_public_ip.test[1].ip_address
 }
 
-output "IP_manager" {
-  value = IP_pub_manager
+output "IP_pub_manager" {
+  value = azurerm_public_ip.test[2].ip_address
 }
