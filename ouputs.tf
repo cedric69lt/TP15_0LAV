48a66e408b21174b9d4c30f25cf951c5912e47a6
lingcode72 <66947285+lingcode72@users.noreply.github.com>
2023-07-17 11:23:26 +0200

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -11,3 +11,15 @@ output "private" {
 output "public_ips" {
   value = [for instance in azurerm_public_ip.test : instance.name]
 }
+
+output "IP_worker_0" {
+  value = P_pub_worker_0
+}
+
+output "IP_worker_1" {
+  value = P_pub_worker_1
+}
+
+output "IP_manager" {
+  value = IP_pub_manager
+}
