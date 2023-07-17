9b56f0f682cdded20afb37072c5bed4c2016e91a
lingcode72 <66947285+lingcode72@users.noreply.github.com>
2023-07-17 11:24:36 +0200

    Update ips_to_inventory.sh

--- ips_to_inventory.sh
+++ ips_to_inventory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 terraform output -raw private > id_rsa
 sudo chmod 400 id_rsa
-sudo echo -e "[masters]\n\nmaster ansible_host=$(terraform output -raw Public_IP-0) ansible_user=azureuser\nmaster ansible_ssh_private_key_file=id_rsa\n\n[workers]\n\nworker0 ansible_host=$(terraform output -raw Public_IP-1) ansible_user=azureuser\nworker0 ansible_ssh_private_key_file=id_rsa\n\nworker1 ansible_host=$(terraform output -raw Public_IP-2) ansible_user=azureuser\nworker1 ansible_ssh_private_key_file=id_rsa" > inventaire.ini
+sudo echo -e "[masters]\n\nmaster ansible_host=$(terraform output -raw IP_manager) ansible_user=momo\nmaster ansible_ssh_private_key_file=id_rsa\n\n[workers]\n\nworker0 ansible_host=$(terraform output -raw IP_worker_0) ansible_user=momo\nworker0 ansible_ssh_private_key_file=id_rsa\n\nworker1 ansible_host=$(terraform output -raw IP_worker_1) ansible_user=momo\nworker1 ansible_ssh_private_key_file=id_rsa" > inventaire.ini
