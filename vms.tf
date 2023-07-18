6f4dd5d31c476f7284cb8cb543514a32c19458ac
obabaldbiyat <127845320+obabaldbiyat@users.noreply.github.com>
2023-07-18 17:46:32 +0200

    Update vms.tf

--- vms.tf
+++ vms.tf
@@ -72,7 +72,7 @@ resource "tls_private_key" "SSH" {
 
     provisioner "file" {
       source = "2048_deployment_replicated.yaml"
-      destination = "/home/momo/manifest.yaml"
+      destination = "/home/azureuser/manifest.yaml"
     }
     depends_on = [var.resource_group_name]
 }
